3672d48b80e2f6dfb9baff53d38892e6210ceea9
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-02-18 15:06:20 +1100

    Add tests for chipbox_cpu_load_program

--- core/testcore.c
+++ core/testcore.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include "core.h"
+#include "cpu.h"
 
 void test(int condition, char* name);
 int all_equal(byte array[], int size, int value);
@@ -9,6 +11,7 @@ int failed = 0;
 
 int main() {
     struct chipbox_chip8_state state;
+    char example_data[CHIPBOX_MEMORY_SIZE - CHIPBOX_PROGRAM_START] = {3, 1, 4, 1, 5, 9, 2};
     /* SECTION 1: typedefs and other low-level administrative stuff */
     test(sizeof(byte) == 1, "byte type should be one byte in length");
     test(sizeof(dbyte) == 2, "dbyte type should be two bytes in length");
@@ -23,6 +26,17 @@ int main() {
     test(state.DT == 0 && state.ST == 0, "delay and sound timers should be set to 0");
     test(state.PC == CHIPBOX_PROGRAM_START, "PC (program counter) should start at specified value");
     test(all_equal(state.input, CHIPBOX_INPUT_KEYS, 0), "all input keys should be set to 0 (unpressed)");
+
+    /* SECTION 3: validating internal cpu functions */
+    state = chipbox_init_state();
+    test(chipbox_cpu_load_program(&state, example_data, 7), "chipbox_cpu_load_program should return a true value on valid input");
+    test(memcmp(&(state.memory[CHIPBOX_PROGRAM_START]), example_data, 7) == 0, "program should be loaded into appropriate memory location");
+    test(state.PC == CHIPBOX_PROGRAM_START, "PC should be set to starting address of loaded program");
+
+    state = chipbox_init_state();
+    test(chipbox_cpu_load_program(&state, example_data, CHIPBOX_MEMORY_SIZE - CHIPBOX_PROGRAM_START), "loading a program of maximum size should be permitted");
+    state = chipbox_init_state();
+    test(chipbox_cpu_load_program(&state, example_data, CHIPBOX_MEMORY_SIZE - CHIPBOX_PROGRAM_START + 1) == 0, "loading a program of too great size should be rejected");
 
     /* END */
     printf("Tests: %d, failed: %d\n", tests, failed);
