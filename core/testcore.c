23919e231c8ff84b01e6abceb445365b5f2ab60d
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-02-27 14:55:40 +1100

    Fix tests for 0x8XYE (SHL VX, VY)

--- core/testcore.c
+++ core/testcore.c
@@ -285,7 +285,7 @@ int main() {
     state.V[0xF] = 50;
     test(chipbox_cpu_eval_opcode(&state, 0x864E), "0x8XYE (SHL VX, VY) should succeed (MATTMIK mode)");
     test(state.V[4] == 0xF0, "0x8XYE (SHL VX, VY) should leave VY unchanged in MATTMIK compatibility mode");
-    test(state.V[6] == 0xF0 << 1, "0x8XYE (SHL VX, VY) should set VX to VY << 1 in MATTMIK compatibility mode");
+    test(state.V[6] == (byte)(0xF0 << 1), "0x8XYE (SHL VX, VY) should set VX to VY << 1 in MATTMIK compatibility mode");
     test(state.V[0xF] == 1, "0x8XYE (SHL VX, VY) should set VF to 1 if most significant bit of VY is 1 (MATTMIK)");
     state = chipbox_init_state();
     state.compat_mode = CHIPBOX_COMPATIBILITY_MODE_MATTMIK;
@@ -301,12 +301,12 @@ int main() {
     state.V[0xF] = 50;
     test(chipbox_cpu_eval_opcode(&state, 0x864E), "0x8XYE (SHL VX, VY) should succeed (COWGOD mode)");
     test(state.V[4] == 0xF0, "0x8XYE (SHL VX, VY) should leave VY unchanged in COWGOD compatibility mode");
-    test(state.V[6] == 0xE0 << 1, "0x8XYE (SHL VX, VY) should set VX to VX << 1 in COWGOD compatibility mode");
+    test(state.V[6] == (byte)(0xE0 << 1), "0x8XYE (SHL VX, VY) should set VX to VX << 1 in COWGOD compatibility mode");
     test(state.V[0xF] == 1, "0x8XYE (SHL VX, VY) should set VF to 1 if most significant bit of VX is 1 (COWGOD)");
     state = chipbox_init_state();
     state.compat_mode = CHIPBOX_COMPATIBILITY_MODE_COWGOD;
-    state.V[4] = 0x0F;
-    state.V[6] = 0xE0;
+    state.V[4] = 0xF0;
+    state.V[6] = 0x0E;
     state.V[0xF] = 50;
     chipbox_cpu_eval_opcode(&state, 0x864E);
     test(state.V[0xF] == 0, "0x8XYE (SHL VX, VY) should set VF to 0 if most significant bit of VX is 0 (COWGOD)");
