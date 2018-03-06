90d67ce6e1edeacb4bcb9a737a7eef42fee5bde4
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-03-06 17:36:39 +1100

    Use Vsync to save resources (which required changes to main loop logic to ensure appropriate speed)

--- chipbox_sdl/chipbox_sdl.c
+++ chipbox_sdl/chipbox_sdl.c
@@ -17,11 +17,13 @@ int main(int argc, char* argv[]) {
     int size_to_read = CHIPBOX_MEMORY_SIZE - CHIPBOX_PROGRAM_START;
     byte file_data[CHIPBOX_MEMORY_SIZE - CHIPBOX_PROGRAM_START];
     unsigned long last_timer_change_time;
-    unsigned long last_time;
-    int ticks;
+    unsigned long new_time;
+    unsigned long current_time;
+    unsigned long delta_time;
     int ticks_per_second = 500;
     int ms_per_tick = 1000 / ticks_per_second;
     int running = 1;
+    int i;
 
     if (argc < 2) {
         printf("Please specify a ROM file\n");
@@ -52,7 +54,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL) {
         printf("Renderer creation failure: %s\n", SDL_GetError());
     }
@@ -60,26 +62,26 @@ int main(int argc, char* argv[]) {
     state = chipbox_init_state();
     chipbox_cpu_load_program(&state, file_data, size_to_read);
     last_timer_change_time = SDL_GetTicks();
+    current_time = SDL_GetTicks();
     while (running) {
-        ticks = 0;
-        do {
-            while(SDL_PollEvent(&e) != 0) {
-                if (e.type == SDL_QUIT) {
-                    running = 0;
-                }
+        new_time = SDL_GetTicks();
+        delta_time = new_time - current_time;
+        current_time = new_time;
+        while(SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                running = 0;
             }
-            chipbox_vm_update_input(&state);
+        }
+        chipbox_vm_update_input(&state);
 
+        for (i = (delta_time * ticks_per_second) / 1000; i > 0; i--) {
             if (!chipbox_vm_step(&state, &last_timer_change_time)) {
                 running = 0;
                 break;
             }
-            chipbox_screen_to_sdl_rects(state.screen, pixel_rects, &pixel_count);
-            chipbox_render(renderer, pixel_rects, pixel_count, CHIPBOX_SCREEN_WIDTH_PIXELS, CHIPBOX_SCREEN_HEIGHT, scale);
-            ticks++;
-            last_time = SDL_GetTicks();
-            while (SDL_GetTicks() - last_time < ms_per_tick); /* busy wait because sleep does not have enough precision */
-        } while (ticks < ticks_per_second);
+        }
+        chipbox_screen_to_sdl_rects(state.screen, pixel_rects, &pixel_count);
+        chipbox_render(renderer, pixel_rects, pixel_count, CHIPBOX_SCREEN_WIDTH_PIXELS, CHIPBOX_SCREEN_HEIGHT, scale);
     }
 
     SDL_DestroyWindow(window);
