4b175160cac6314856217da836c6a33d00a45ce7
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-05-29 17:15:12 +1000

    Add das test for 0x00EE (RET)

--- src/tests/testdas.c
+++ src/tests/testdas.c
@@ -10,6 +10,10 @@ int main() {
     test(strcmp(info.mnemonic, "CLS") == 0, "0x00E0 should be CLS");
     test(info.args == 0, "0x00E0 (CLS) should have no arguments");
 
+    info = disassemble_instruction(0x00EE);
+    test(strcmp(info.mnemonic, "RET") == 0, "0x00EE should be RET");
+    test(info.args == 0, "0x00EE (RET) should have no arguments");
+
     print_end();
     return 0;
 }
