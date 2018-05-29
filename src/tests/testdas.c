4cffa7ae9a700407e4e8479ae01ecfcb22b8499b
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-05-29 17:12:37 +1000

    Add das test for 0x00E0 (CLS)

--- src/tests/testdas.c
+++ src/tests/testdas.c
@@ -1,8 +1,14 @@
 #include "core.h"
 #include "dascore.h"
 #include "testhelpers.h"
+#include <string.h>
 
 int main() {
+    struct chipbox_instruction_info info;
+
+    info = disassemble_instruction(0x00E0);
+    test(strcmp(info.mnemonic, "CLS") == 0, "0x00E0 should be CLS");
+    test(info.args == 0, "0x00E0 (CLS) should have no arguments");
 
     print_end();
     return 0;
