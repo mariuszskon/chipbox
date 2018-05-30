aee700a7527ef114298cf4fb3136880fb6e39e06
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-05-30 19:36:30 +1000

    Simplify bitwise operation

--- src/das/dascore.c
+++ src/das/dascore.c
@@ -5,7 +5,7 @@
 struct chipbox_instruction_info disassemble_instruction(dbyte instruction) {
     struct chipbox_instruction_info info;
 
-    switch ((instruction & 0xF000) >> 12) { /* get highest/left-most nybble */
+    switch (instruction >> 12) { /* get highest/left-most nybble */
 
     }
 
