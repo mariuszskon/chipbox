1cb6f3f95c52a00c0964b5b3a43b56bdf5793128
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-05-30 19:38:57 +1000

    Implement disassembly of 0x00E0 (CLS)

--- src/das/dascore.c
+++ src/das/dascore.c
@@ -6,7 +6,13 @@ struct chipbox_instruction_info disassemble_instruction(dbyte instruction) {
     struct chipbox_instruction_info info;
 
     switch (instruction >> 12) { /* get highest/left-most nybble */
-
+        case 0:
+            switch (instruction) {
+                case 0x00E0:
+                    strcpy(info.mnemonic, "CLS");
+                    info.num_args = 0;
+                    return info;
+            }
     }
 
     /* if we have not returned yet, the instruction was unknown */
