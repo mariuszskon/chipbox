14d8f7eacafbd2a745dadf0028d04490037fb01b
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2018-05-30 17:02:04 +1000

    Use a shorthand macro for disassembling a single instruction

--- src/tests/testdas.c
+++ src/tests/testdas.c
@@ -3,32 +3,33 @@
 #include "testhelpers.h"
 #include <string.h>
 
+#define DASI(DBYTE) (info = disassemble_instruction(DBYTE))
 #define MEQ(MNEMONIC) (strcmp(info.mnemonic, MNEMONIC) == 0)
 
 int main() {
     struct chipbox_instruction_info info;
 
-    info = disassemble_instruction(0x00E0);
+    DASI(0x00E0);
     test(MEQ("CLS"), "0x00E0 should be CLS");
     test(info.num_args == 0, "0x00E0 (CLS) should have no arguments");
 
-    info = disassemble_instruction(0x00EE);
+    DASI(0x00EE);
     test(MEQ("RET"), "0x00EE should be RET");
     test(info.num_args == 0, "0x00EE (RET) should have no arguments");
 
-    info = disassemble_instruction(0x01EE);
+    DASI(0x01EE);
     test(MEQ("SYS"), "0x0NNN should be SYS if not CLS or RET");
     test(info.num_args == 1 && strcmp(info.args[0], "1EE") == 0, "0x0NNN (SYS NNN) should have one argument: NNN");
 
-    info = disassemble_instruction(0x1234);
+    DASI(0x1234);
     test(MEQ("JP"), "0x1NNN should be JP");
     test(info.num_args == 1 && strcmp(info.args[0], "234") == 0, "0x1NNN (JP NNN) should have one argument: NNN");
 
-    info = disassemble_instruction(0x2456);
+    DASI(0x2456);
     test(MEQ("CALL"), "0x2NNN should be CALL");
     test(info.num_args == 1 && strcmp(info.args[0], "456") == 0, "0x2NNN (CALL NNN) should have one argument: NNN");
 
-    info = disassemble_instruction(0x35AB);
+    DASI(0x35AB);
     test(MEQ("SE"), "0x3XNN should be SE");
     test(info.num_args == 2 && strcmp(info.args[0], "V5") == 0 && strcmp(info.args[1], "AB") == 0, "0x3XNN (SE VX, NN) should have two arguments: VX and NN");
 
